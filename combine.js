161687c3d3e7cd416461f8689bea07583007c578
Harsha Kata <harshakata@Harshas-MBP.attlocal.net>
2024-09-04 16:33:27 -0700

    added docker image links

--- combine.js
+++ combine.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 async function getCombinedMessage() {
     try {
-        const helloResponse = await axios.get('http://127.0.0.1:59669/hello'); /* alternatively 'http://localhost:3000/hello' can also be used*/
-        const worldResponse = await axios.get('http://127.0.0.1:59692/world'); /* alternatively 'http://localhost:3001/world' can also be used*/
+        const helloResponse = await axios.get('http://localhost:3000/hello'); /* alternatively 'http://127.0.0.1:59669/hello' can also be used - this url is returned when 'minikube service hello' is executed */
+        const worldResponse = await axios.get('http://localhost:3001/world'); /* alternatively 'http://127.0.0.1:59692/world' can also be used - this url is returned when 'minikube service world' is executed */
         console.log(`${helloResponse.data} ${worldResponse.data}`);
     } catch (error) {
         console.error('Error fetching messages:', error);
